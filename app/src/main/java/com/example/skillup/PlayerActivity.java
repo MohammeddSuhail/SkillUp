fefb10014c31ddbe07c4b4c72510325a69b67a74
Prathamesh <prathameshspai05@gmail.com>
2022-11-09 16:19:36 +0530

    Youtube Player changed!

--- app/src/main/java/com/example/skillup/PlayerActivity.java
+++ app/src/main/java/com/example/skillup/PlayerActivity.java
@@ -1,48 +1,72 @@
 package com.example.skillup;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
 
-public class PlayerActivity extends YouTubeBaseActivity {
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
-    YouTubePlayerView youTubePlayerView;
+public class PlayerActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+        getSupportActionBar().hide();
 
         String[] vid = getIntent().getStringArrayExtra("video");
         TextView t = (TextView) findViewById(R.id.vtitle);
+        YouTubePlayerView ypv = findViewById(R.id.youtube_player_view);
+
+
         String wholeThing = "";
         for (int i = 0; i < vid.length; i++) {
             wholeThing += vid[i] +"\n";
         }
         t.setText(vid[2]);
 
-        YouTubePlayerView ypv = findViewById(R.id.youtube_player_view);
-        YouTubePlayer.OnInitializedListener listener = new YouTubePlayer.OnInitializedListener() {
+        getLifecycle().addObserver(ypv);
+        ypv.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
             @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                youTubePlayer.loadVideo(vid[0]);
-                youTubePlayer.play();
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = vid[0];
+                youTubePlayer.loadVideo(videoId, 0);
+                Log.d("Jj", "Thomson");
             }
+        });
 
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                Toast.makeText(getApplicationContext(),
-                        "Failed",
-                        Toast.LENGTH_SHORT).show();
-            }
-        };
-        ypv.initialize("AIzaSyBmNsfFLi8vqW2ZZE-ZRJTUd5sfRnw74bQ",listener);
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ypv.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        }
+
+
+
+
+//        YouTubePlayer.OnInitializedListener listener = new YouTubePlayer.OnInitializedListener() {
+//            @Override
+//            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//                youTubePlayer.loadVideo(vid[0]);
+//                youTubePlayer.play();
+//            }
+//
+//            @Override
+//            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//                Toast.makeText(getApplicationContext(),
+//                        "Failed",
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//        ypv.initialize("AIzaSyBmNsfFLi8vqW2ZZE-ZRJTUd5sfRnw74bQ",listener);
 
 
 
