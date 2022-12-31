88a09a515228452cb5581c07c5f02b375f32b8a8
Prathamesh <prathameshspai05@gmail.com>
2022-12-31 21:26:09 +0530

    Top contributors added

---
+++ app/src/main/java/com/example/skillup/ContriDetailActivity.java
@@ -0,0 +1,100 @@
+package com.example.skillup;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+public class ContriDetailActivity extends AppCompatActivity {
+
+    String name, ctc, companies_placed, cgpa, core;
+    TextView nametv, company_listtv, Cgpatv, Coretv;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contri_detail);
+
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#121212"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>Top Contributor</font>"));
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+
+        img = findViewById(R.id.image);
+
+        if(Objects.equals(name, "Prathamesh S Pai")){
+            ctc="15 Lakhs";
+            companies_placed = "• Informatica (CTC: 15 Lakhs)\n• TCS (MRC - CTC: 3.3 Lakhs)";
+            cgpa = "8.50";
+            core = "Mobile Application Development";
+            img.setImageResource(R.drawable.me);
+        }
+
+        if(Objects.equals(name, "Sabarinath")){
+            ctc="22 Lakhs";
+            companies_placed = "• Cohesity (CTC: 22 Lakhs)\n• Tismo (CTC: 7.25 Lakhs)";
+            cgpa = "8.88";
+            core = "• Web Development (MERN)\n• Cloud Computing";
+            img.setImageResource(R.drawable.sab);
+        }
+
+        if(Objects.equals(name, "R Anuroop")){
+            ctc="14.5 Lakhs";
+            companies_placed = "Capillary Technologies (CTC: 14.5 Lakhs)";
+            cgpa = "9.61";
+            core = "• Web Development\n• Blockchain";
+            img.setImageResource(R.drawable.anuroop);
+        }
+
+        if(Objects.equals(name, "Ranjan Upadhya")){
+            ctc="14.5 Lakhs";
+            companies_placed = "Daimler Truck (CTC: 10 Lakhs)";
+            cgpa = "9.17";
+            core = "• Web Development\n• Java Programming";
+            img.setImageResource(R.drawable.ranjan);
+        }
+
+        if(Objects.equals(name, "Mohammed Suhail")){
+            ctc="12 Lakhs";
+            companies_placed = "ACI Worldwide (CTC: 12 Lakhs)";
+            cgpa = "9.39";
+            core = "Android Development";
+            img.setImageResource(R.drawable.suhail);
+        }
+
+        if(Objects.equals(name, "Ranjith Kumar R")){
+            ctc="12 Lakhs";
+            companies_placed = "• Daimler Truck (CTC: 10 Lakhs)\n• Accenture (MRC - CTC: 4.5 Lakhs)\n• Capgemini (MRC - CTC: 4.25 Lakhs)\n• DXC (MRC - CTC: 4.2 Lakhs)";
+            cgpa = "9.31";
+            core = "Web Development";
+            img.setImageResource(R.drawable.ran);
+        }
+
+        nametv = findViewById(R.id.name);
+        company_listtv=findViewById(R.id.company_list);
+        Cgpatv=findViewById(R.id.Cgpa);
+        Coretv=findViewById(R.id.CoreCompetency);
+
+        nametv.setText(name);
+        company_listtv.setText(companies_placed);
+        Coretv.setText(core);
+        Cgpatv.setText(cgpa);
+
+    }
+}
--- app/src/main/java/com/example/skillup/ContributorActivity.java
+++ app/src/main/java/com/example/skillup/ContributorActivity.java
@@ -2,13 +2,18 @@ package com.example.skillup;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 
 public class ContributorActivity extends AppCompatActivity {
+
+    CardView prathamesh, suhail, ranjan, ranjith, anuroop, sabarinath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +29,61 @@ public class ContributorActivity extends AppCompatActivity {
         // Set BackgroundDrawable
         actionBar.setBackgroundDrawable(colorDrawable);
         actionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>Top Contributors</font>"));
+
+        prathamesh = findViewById(R.id.prathamesh);
+        suhail = findViewById(R.id.suhail);
+        ranjan = findViewById(R.id.ranjan);
+        ranjith = findViewById(R.id.ranjith);
+        anuroop = findViewById(R.id.anuroop);
+        sabarinath = findViewById(R.id.sabarinath);
+
+        prathamesh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "Prathamesh S Pai");
+                startActivity(i1);
+            }
+        });
+        suhail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "Mohammed Suhail");
+                startActivity(i1);
+            }
+        });
+        ranjan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "Ranjan Upadhya");
+                startActivity(i1);
+            }
+        });
+        sabarinath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "Sabarinath");
+                startActivity(i1);
+            }
+        });
+        ranjith.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "Ranjith Kumar R");
+                startActivity(i1);
+            }
+        });
+        anuroop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(ContributorActivity.this, ContriDetailActivity.class);
+                i1.putExtra("name", "R Anuroop");
+                startActivity(i1);
+            }
+        });
     }
 }
