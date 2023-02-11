9be2e85f0657fa124d9915f62dce970d9e05b782
Prajith-Shetty <PrajithShettyJ@gmail.com>
2023-02-11 09:58:19 +0530

    minor changes

--- app/src/main/java/com/example/skillup/DSASheet.java
+++ app/src/main/java/com/example/skillup/DSASheet.java
@@ -10,5 +10,7 @@ public class DSASheet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dsasheet);
+
+        getSupportActionBar().hide();
     }
 }
