a4dc64efe80ccc77f8ba333bb7ec7a2218159179
Mohammed Suhail <moh3suhail@gmail.com>
2023-05-02 16:15:12 +0530

    res

--- app/src/main/java/com/codingchallengers/skillup/resSkillActivity.java
+++ app/src/main/java/com/codingchallengers/skillup/resSkillActivity.java
@@ -9,6 +9,6 @@ public class resSkillActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_res_skill2);
+        setContentView(R.layout.activity_res_skill);
     }
 }
