3933c6f3a73a1236da90869936f3fc095b421e1a
Mohammed Suhail <moh3suhail@gmail.com>
2023-05-02 20:29:44 +0530

    res

--- app/src/main/java/com/codingchallengers/skillup/fragments/CoursesFragment.java
+++ app/src/main/java/com/codingchallengers/skillup/fragments/CoursesFragment.java
@@ -1,5 +1,6 @@
 package com.codingchallengers.skillup.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -12,14 +13,16 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.codingchallengers.skillup.EditDetailsActivity;
 import com.codingchallengers.skillup.R;
+import com.codingchallengers.skillup.resSkillActivity;
 
 public class CoursesFragment extends Fragment {
 
     public static FragmentTransaction f;
 
 
-    CardView cn,dbms,os,oops, dsa, ie, res;
+    CardView cn,dbms,os,oops, dsa, ie, res, rescardviewBuilder;
 
     public static String course;
 
@@ -43,6 +46,7 @@ public class CoursesFragment extends Fragment {
         dsa=(CardView) view.findViewById(R.id.dsacardview);
         ie = (CardView) view.findViewById(R.id.ie_cardview);
         res = (CardView) view.findViewById(R.id.rescardview);
+        rescardviewBuilder = (CardView) view.findViewById(R.id.rescardviewBuilder);
 
 
         CoursesFragment cf = new CoursesFragment();
@@ -108,6 +112,16 @@ public class CoursesFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 CoursesFragment.f.replace(R.id.fragment_container, new IntExpListFragment()).addToBackStack(null).commit();
+            }
+        });
+
+
+        rescardviewBuilder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EditDetailsActivity.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
 
