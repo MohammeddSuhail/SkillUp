b72ef8a5143316096d20fc7e47ff791c507050b0
nagaraj2507 <92426290+nagaraj2507@users.noreply.github.com>
2023-05-03 21:24:26 +0530

    Changes to toolbar of resume

--- app/src/main/java/com/codingchallengers/skillup/resEduActivity.java
+++ app/src/main/java/com/codingchallengers/skillup/resEduActivity.java
@@ -3,7 +3,10 @@ package com.codingchallengers.skillup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +21,9 @@ public class resEduActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_edu);
+
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>SkillUp</font>"));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2a2b2d")));
 
         degree_et = findViewById(R.id.form_edu_et_degree);
         university_et = findViewById(R.id.form_edu_et_university);
--- app/src/main/java/com/codingchallengers/skillup/resProjActivity.java
+++ app/src/main/java/com/codingchallengers/skillup/resProjActivity.java
@@ -3,7 +3,10 @@ package com.codingchallengers.skillup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,6 +21,9 @@ public class resProjActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_proj);
+
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>SkillUp</font>"));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2a2b2d")));
 
         title_et = findViewById(R.id.form_pro_et_title);
         desc_et = findViewById(R.id.form_pro_et_desc);
